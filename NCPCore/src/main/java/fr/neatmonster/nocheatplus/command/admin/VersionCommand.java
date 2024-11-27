31218efbdfc3428c3818ab19f268f977d520255b
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-11-27 03:04:53 -0500

    Clean up

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/command/admin/VersionCommand.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/command/admin/VersionCommand.java
@@ -56,83 +56,14 @@ public class VersionCommand extends BaseCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        final Component info = getVersionInfoNew();
+        final Component info = getVersionInfo();
 
         NCPAPIProvider.getNoCheatPlusAPI().adventure().sender(sender).sendMessage(info);
 
         return true;
     }
 
-//    public static List<String> getVersionInfo() {
-//        final ChatColor c1, c2, c3, c4, c5, c6, c7;
-//        //if (sender instanceof Player) {
-//        c1 = ChatColor.GRAY;
-//        c2 = ChatColor.BOLD;
-//        c3 = ChatColor.RED;
-//        c4 = ChatColor.ITALIC;
-//        c5 = ChatColor.GOLD;
-//        c6 = ChatColor.WHITE;
-//        c7 = ChatColor.YELLOW;
-//        //}
-//        //else {
-//        //    c1 = c2 = c3 = c4 = c5 = c6 = c7 = null;
-//        //}
-//
-//        final List<String> lines = new LinkedList<String>();
-//        final MCAccess mcAccess = NCPAPIProvider.getNoCheatPlusAPI().getGenericInstance(MCAccess.class);
-//        lines.addAll(Arrays.asList(new String[]{
-//                c3 +""+ c2 + "»Version information«" + c1,
-//                c5 +""+ c2 + "Server:" + c1,
-//                c1 + alt(Bukkit.getServer().getVersion()),
-//                c1 +""+ c7 + "Detected: " + c1 + alt(ServerVersion.getMinecraftVersion()),
-//                c5 +""+ c2 + "NoCheatPlus:" + c1,
-//                c1 +""+ c7 + "Plugin: "+ c1 + alt(Bukkit.getPluginManager().getPlugin("NoCheatPlus").getDescription().getVersion()),
-//                c1 +""+ c7 +  "MCAccess: " + c1 + alt(mcAccess.getMCVersion() + " / " + mcAccess.getServerVersionTag()),
-//        }));
-//
-//        final Map<String, Set<String>> featureTags = NCPAPIProvider.getNoCheatPlusAPI().getAllFeatureTags();
-//        if (!featureTags.isEmpty()) {
-//            final List<String> features = new LinkedList<String>();
-//            // Add present features.
-//            for (final Entry<String, Set<String>> entry : featureTags.entrySet()) {
-//                features.add(alt(c1 +""+ c7 +""+ entry.getKey() + c1 + ": " + StringUtil.join(entry.getValue(), c6 + ", " + c1)));
-//            }
-//            // Sort and add.
-//            Collections.sort(features, String.CASE_INSENSITIVE_ORDER);
-//            features.add(0, c5 +""+ c2 +"Features:");
-//            lines.addAll(features);
-//        }
-//
-//        final Collection<NCPHook> hooks = NCPHookManager.getAllHooks();
-//        if (!hooks.isEmpty()){
-//            final List<String> fullNames = new LinkedList<String>();
-//            for (final NCPHook hook : hooks){
-//                fullNames.add(alt(hook.getHookName() + " " + hook.getHookVersion()));
-//            }
-//            Collections.sort(fullNames, String.CASE_INSENSITIVE_ORDER);
-//            lines.add(c5 +""+ c2 + "Hooks:\n" + c1 + StringUtil.join(fullNames, c6 + ", " + c1));
-//        }
-//
-//        final List<String> relatedPlugins = new LinkedList<String>();
-//        for (final String name : new String[]{"CompatNoCheatPlus", "ProtocolLib", "ViaVersion", "ProtocolSupport", "PNCP", "NTAC"}) {
-//            Plugin plugin = Bukkit.getPluginManager().getPlugin(name);
-//            if (plugin != null) {
-//                relatedPlugins.add(alt(plugin.getDescription().getFullName()));
-//            }
-//        }
-//
-//        if (!relatedPlugins.isEmpty()) {
-//            lines.add(c3 +""+ c2 + "»Related Plugins«" + c1);
-//            lines.add(c1 +""+ StringUtil.join(relatedPlugins, c6 + ", " + c1));
-//        }
-//        return lines;
-//    }
-//
-//    private static String alt(String x) {
-//        return x.replace('(', '~').replace(')', '~');
-//    }
-
-    public static Component getVersionInfoNew() {
+    public static Component getVersionInfo() {
         final TextComponent.@NotNull Builder info = Component.text();
 
         final TextComponent.@NotNull Builder featureTagsComponent = Component.text();
@@ -208,7 +139,7 @@ public class VersionCommand extends BaseCommand {
     }
 
     public static String getFormattedVersionInfo() {
-        final Component formatted = getVersionInfoNew().replaceText(TextReplacementConfig.builder().matchLiteral("\n").replacement(Component.newline()).build());
+        final Component formatted = getVersionInfo().replaceText(TextReplacementConfig.builder().matchLiteral("\n").replacement(Component.newline()).build());
 
         return PlainTextComponentSerializer.plainText().serialize(formatted);
     }
