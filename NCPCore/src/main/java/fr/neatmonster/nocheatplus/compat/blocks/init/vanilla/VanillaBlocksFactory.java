06236cd29ae1e054030335f02775b82a3fa3e87a
AnarchadiaMC <166779226+AnarchadiaMC@users.noreply.github.com>
2025-03-03 19:56:55 -0600

    1.21 Update

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/compat/blocks/init/vanilla/VanillaBlocksFactory.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/compat/blocks/init/vanilla/VanillaBlocksFactory.java
@@ -46,6 +46,7 @@ public class VanillaBlocksFactory {
             setups.add(new BlocksMC1_17());
             setups.add(new BlocksMC1_19());
             setups.add(new BlocksMC1_20());
+            setups.add(new BlocksMC1_21());
         }
         catch(Throwable t){}
         for (final BlockPropertiesSetup setup : setups){
