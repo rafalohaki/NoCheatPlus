f47006369bf7a7b27d2d3b308440ca3a561a2164
rafalohaki <rafalohaki@gmail.com>
2025-06-25 18:11:44 +0000

    fix

--- NCPCompatBukkit/src/test/java/fr/neatmonster/nocheatplus/compat/bukkit/TestBukkitAttributeAccess.java
+++ NCPCompatBukkit/src/test/java/fr/neatmonster/nocheatplus/compat/bukkit/TestBukkitAttributeAccess.java
@@ -8,37 +8,49 @@ import java.util.Collections;
 
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
-import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
+import org.junit.Assume;
 import org.junit.Test;
 
 public class TestBukkitAttributeAccess {
 
     @Test
     public void testGetSpeedAttributeMultiplierNull() {
+        // Get instance via factory method.
+        BukkitAttributeAccess access = BukkitAttributeAccess.createIfSupported();
+        // Skip this test if the class is not supported in the test environment.
+        Assume.assumeNotNull(access);
+        
         Player player = mock(Player.class);
         when(player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)).thenReturn(null);
-        BukkitAttributeAccess access = new BukkitAttributeAccess();
         assertEquals(Double.MAX_VALUE, access.getSpeedAttributeMultiplier(player), 0.0);
     }
 
     @Test
     public void testGetSprintAttributeMultiplierNull() {
+        // Get instance via factory method.
+        BukkitAttributeAccess access = BukkitAttributeAccess.createIfSupported();
+        // Skip this test if the class is not supported in the test environment.
+        Assume.assumeNotNull(access);
+        
         Player player = mock(Player.class);
         when(player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)).thenReturn(null);
-        BukkitAttributeAccess access = new BukkitAttributeAccess();
         assertEquals(Double.MAX_VALUE, access.getSprintAttributeMultiplier(player), 0.0);
     }
 
     @Test
     public void testNormalSpeed() {
+        // Get instance via factory method.
+        BukkitAttributeAccess access = BukkitAttributeAccess.createIfSupported();
+        // Skip this test if the class is not supported in the test environment.
+        Assume.assumeNotNull(access);
+        
         Player player = mock(Player.class);
         AttributeInstance inst = mock(AttributeInstance.class);
         when(player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)).thenReturn(inst);
         when(inst.getValue()).thenReturn(0.1);
         when(inst.getBaseValue()).thenReturn(0.1);
         when(inst.getModifiers()).thenReturn(Collections.emptySet());
-        BukkitAttributeAccess access = new BukkitAttributeAccess();
         assertEquals(1.0, access.getSpeedAttributeMultiplier(player), 0.0);
         assertEquals(1.0, access.getSprintAttributeMultiplier(player), 0.0);
     }
--- NCPCompatBukkit/src/test/java/fr/neatmonster/nocheatplus/compat/bukkit/TestNSBukkitAttributeAccess.java
+++ NCPCompatBukkit/src/test/java/fr/neatmonster/nocheatplus/compat/bukkit/TestNSBukkitAttributeAccess.java
@@ -9,35 +9,48 @@ import java.util.Collections;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
+import org.junit.Assume;
 import org.junit.Test;
 
 public class TestNSBukkitAttributeAccess {
 
     @Test
     public void testGetSpeedAttributeMultiplierNull() {
+        // Get instance via factory method.
+        NSBukkitAttributeAccess access = NSBukkitAttributeAccess.createIfSupported();
+        // Skip this test if the class is not supported in the test environment.
+        Assume.assumeNotNull(access);
+        
         Player player = mock(Player.class);
         when(player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)).thenReturn(null);
-        NSBukkitAttributeAccess access = new NSBukkitAttributeAccess();
         assertEquals(Double.MAX_VALUE, access.getSpeedAttributeMultiplier(player), 0.0);
     }
 
     @Test
     public void testGetSprintAttributeMultiplierNull() {
+        // Get instance via factory method.
+        NSBukkitAttributeAccess access = NSBukkitAttributeAccess.createIfSupported();
+        // Skip this test if the class is not supported in the test environment.
+        Assume.assumeNotNull(access);
+        
         Player player = mock(Player.class);
         when(player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)).thenReturn(null);
-        NSBukkitAttributeAccess access = new NSBukkitAttributeAccess();
         assertEquals(Double.MAX_VALUE, access.getSprintAttributeMultiplier(player), 0.0);
     }
 
     @Test
     public void testNormalSpeed() {
+        // Get instance via factory method.
+        NSBukkitAttributeAccess access = NSBukkitAttributeAccess.createIfSupported();
+        // Skip this test if the class is not supported in the test environment.
+        Assume.assumeNotNull(access);
+        
         Player player = mock(Player.class);
         AttributeInstance inst = mock(AttributeInstance.class);
         when(player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)).thenReturn(inst);
         when(inst.getValue()).thenReturn(0.1);
         when(inst.getBaseValue()).thenReturn(0.1);
         when(inst.getModifiers()).thenReturn(Collections.emptySet());
-        NSBukkitAttributeAccess access = new NSBukkitAttributeAccess();
         assertEquals(1.0, access.getSpeedAttributeMultiplier(player), 0.0);
         assertEquals(1.0, access.getSprintAttributeMultiplier(player), 0.0);
     }
