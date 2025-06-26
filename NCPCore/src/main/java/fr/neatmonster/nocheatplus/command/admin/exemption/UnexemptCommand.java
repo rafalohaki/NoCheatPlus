d7c262bd3515d9bdfcad76dd22f98cda4d357da7
rafalohaki <rafalohaki@gmail.com>
2025-06-26 11:18:20 +0200

    Add color helper and refactor exemption commands

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/command/admin/exemption/UnexemptCommand.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/command/admin/exemption/UnexemptCommand.java
@@ -22,7 +22,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.ChatColor;
 
 import fr.neatmonster.nocheatplus.checks.CheckType;
 import fr.neatmonster.nocheatplus.command.BaseCommand;
@@ -41,7 +40,6 @@ public class UnexemptCommand extends BaseCommand {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        // Consider introducing a super class to reduce copy and paste.
         if (args.length < 2) {
             sender.sendMessage((sender instanceof Player ? TAG : CTAG) + "Please specify a player to unexempt.");
             return true;
@@ -95,10 +93,10 @@ public class UnexemptCommand extends BaseCommand {
         try {
             return CheckType.valueOf(input.toUpperCase().replace('-', '_').replace('.', '_'));
         } catch (Exception e) {
-            final boolean player = sender instanceof Player;
-            final String tag = player ? TAG : CTAG;
-            final String c3 = player ? ChatColor.RED.toString() : "";
-            final String c6 = player ? ChatColor.WHITE.toString() : "";
+            final String tag = sender instanceof Player ? TAG : CTAG;
+            final String[] colors = getColorCodes(sender);
+            final String c3 = colors[2];
+            final String c6 = colors[5];
             sender.sendMessage(tag + "Could not interpret: " + c3 + input);
             sender.sendMessage(tag + "Check type should be one of: " + c3 + StringUtil.join(Arrays.asList(CheckType.values()), c6 + ", " + c3));
             return null;
@@ -115,18 +113,18 @@ public class UnexemptCommand extends BaseCommand {
 
     private void unexemptAll(CheckType type, CommandSender sender) {
         NCPExemptionManager.clear();
-        final boolean player = sender instanceof Player;
-        final String tag = player ? TAG : CTAG;
-        final String c3 = player ? ChatColor.RED.toString() : "";
+        final String tag = sender instanceof Player ? TAG : CTAG;
+        final String[] colors = getColorCodes(sender);
+        final String c3 = colors[2];
         sender.sendMessage(tag + "Removed exemptions for all players for checks: " + c3 + type);
     }
 
     private void unexemptPlayer(UUID id, String playerName, CheckType type, CommandSender sender) {
         NCPExemptionManager.unexempt(id, type);
-        final boolean player = sender instanceof Player;
-        final String tag = player ? TAG : CTAG;
-        final String c1 = player ? ChatColor.GRAY.toString() : "";
-        final String c3 = player ? ChatColor.RED.toString() : "";
+        final String tag = sender instanceof Player ? TAG : CTAG;
+        final String[] colors = getColorCodes(sender);
+        final String c1 = colors[0];
+        final String c3 = colors[2];
         sender.sendMessage(tag + "Removed exemptions for " + c3 + playerName + c1 + " for checks: " + c3 + type);
     }
 
