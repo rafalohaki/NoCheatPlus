e6b4068aca5d5a90b04d01aedeaa631a4b1943b4
rafalohaki <rafalohaki@gmail.com>
2025-06-28 16:58:41 +0200

    Remove redundant interface implementation

--- NCPCommons/src/main/java/fr/neatmonster/nocheatplus/utilities/ds/map/LinkedCoordHashMap.java
+++ NCPCommons/src/main/java/fr/neatmonster/nocheatplus/utilities/ds/map/LinkedCoordHashMap.java
@@ -31,7 +31,7 @@ import java.util.NoSuchElementException;
  *
  * @param <V>
  */
-public class LinkedCoordHashMap<V> extends AbstractCoordHashMap<V, fr.neatmonster.nocheatplus.utilities.ds.map.LinkedCoordHashMap.LinkedHashEntry<V>> implements CoordMap<V> {
+public class LinkedCoordHashMap<V> extends AbstractCoordHashMap<V, fr.neatmonster.nocheatplus.utilities.ds.map.LinkedCoordHashMap.LinkedHashEntry<V>> {
 
     // Add default order for get/put?
     // Tests pending.
